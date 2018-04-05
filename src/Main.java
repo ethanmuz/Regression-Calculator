327d99458b7d2b47bcb950600f6b75ecd078b2d0
Ethan Uzarowski <ethanmuz@gmail.com>
2018-04-05 10:10:55 -0400

    Added comments in main class

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,24 @@ import java.util.*;
 
 public class Main {
     public static void main(String[]args){
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);  // Scanner to retrieve data from command line
 
         System.out.println("Please enter each datapoint as \"x,y\". When you are done, please type \"done\"");
 
-        ArrayList<Double> x = new ArrayList<>();
-        ArrayList<Double> y = new ArrayList<>();
-        String entry = scan.nextLine();
-        while (!entry.equals("done") && !entry.equals("Done")){
-            x.add(Double.parseDouble(entry.replace(" ","").split(",")[0]));
-            y.add(Double.parseDouble(entry.replace(" ","").split(",")[1]));
-            entry = scan.nextLine();
+        ArrayList<Double> x = new ArrayList<>();    // ArrayList to hold the x values of the plot
+        ArrayList<Double> y = new ArrayList<>();    // ArrayList to hold the y values of the plot
+        String entry = scan.nextLine(); // Get the first datapoint from the user
+        while (!entry.equals("done") && !entry.equals("Done")){ // Continue accepting datapoints until the user enters "done"
+            x.add(Double.parseDouble(entry.replace(" ","").split(",")[0])); // Parse the x value
+            y.add(Double.parseDouble(entry.replace(" ","").split(",")[1])); // Parse the y value
+            entry = scan.nextLine();    // Accept a new point
         }
 
         // x = {};
         // y = {};
 
-        LinearRegression LR = new LinearRegression(x, y);
-        System.out.println("y = " + LR.getSlope() + "x + " + LR.getyIntercept());
-        System.out.println("r^2 = " + LR.getR2());
+        LinearRegression LR = new LinearRegression(x, y);   // Pass the x and y values to LinearRegression class
+        System.out.println("y = " + LR.getSlope() + "x + " + LR.getyIntercept());   // Return the regression line y=mx+b
+        System.out.println("r^2 = " + LR.getR2());  // Return the r^2 value
     }
 }
