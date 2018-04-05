b587f7254f6a29ea68a3900f4320e809bc210e68
Ethan Uzarowski <ethanmuz@gmail.com>
2018-04-05 09:54:27 -0400

    Now you can enter datapoints as x,y pairs

--- src/LinearRegression.java
+++ src/LinearRegression.java
@@ -1,85 +1,87 @@
+import java.util.ArrayList;
+
 public class LinearRegression {
-    private double[] x;
-    private double[] y;
+    private ArrayList<Double> x;
+    private ArrayList<Double> y;
 
-    private double slope;
-    private double yIntercept;
-    private double r;
+    private Double slope;
+    private Double yIntercept;
+    private Double r;
 
-    private double xbar;
-    private double ybar;
-    private double Sx;
-    private double Sy;
+    private Double xbar;
+    private Double ybar;
+    private Double Sx;
+    private Double Sy;
 
-    public LinearRegression(double[] x, double[] y){
+    LinearRegression(ArrayList<Double> x, ArrayList<Double> y){
         this.x = x; this.y = y;
         findXbar(); findYbar(); findSx(); findSy(); findR();
         findSlope(); findYintercept();
     }
 
     private void findXbar(){
-        double total = 0.0;
-        for (double i: x)
+        Double total = 0.0;
+        for (Double i: x)
             total+=i;
-        xbar = total/x.length;
+        xbar = total/x.size();
     }
 
     private void findSx(){
-        double total = 0.0;
-        for (double i: x)
+        Double total = 0.0;
+        for (Double i: x)
             total+=Math.pow(i-xbar,2);
-        Sx = Math.sqrt(total/(x.length-1));
+        Sx = Math.sqrt(total/(x.size()-1));
     }
 
     private void findSy(){
-        double total = 0.0;
-        for (double i: y)
+        Double total = 0.0;
+        for (Double i: y)
             total+=Math.pow(i-ybar,2);
-        Sy = Math.sqrt(total/(y.length-1));
+        Sy = Math.sqrt(total/(y.size()-1));
     }
 
     private void findYbar(){
-        double total = 0.0;
-        for (double i: y)
+        Double total = 0.0;
+        for (Double i: y)
             total+=i;
-        ybar = total/y.length;
+        ybar = total/y.size();
     }
 
     private void findR(){
-        double numerator = 0.0;
-        for (int i = 0; i < x.length; i++)
-            numerator += (x[i]-xbar)*(y[i]-ybar);
+        Double numerator = 0.0;
+        for (int i = 0; i < x.size(); i++)
+            numerator += (x.get(i)-xbar)*(y.get(i)-ybar);
 
-        double xMinusXbar2 = 0.0;
-        for (double i: x)
+        Double xMinusXbar2 = 0.0;
+        for (Double i: x)
             xMinusXbar2+=Math.pow(i-xbar,2);
 
-        double yMinusYbar2 = 0.0;
-        for (double i: y)
+        Double yMinusYbar2 = 0.0;
+        for (Double i: y)
             yMinusYbar2+=Math.pow(i-ybar,2);
 
-        double denominator = Math.sqrt(xMinusXbar2 * yMinusYbar2);
+        Double denominator = Math.sqrt(xMinusXbar2 * yMinusYbar2);
 
         r = numerator/denominator;
     }
 
-    public void findSlope(){
+    private void findSlope(){
         slope = r * (Sy/Sx);
     }
 
-    public void findYintercept(){
+    private void findYintercept(){
         yIntercept = ybar - (slope*xbar);
     }
 
-    public double getSlope(){
+    public Double getSlope(){
         return slope;
     }
 
-    public double getyIntercept(){
+    public Double getyIntercept(){
         return yIntercept;
     }
 
-    public double getR2(){
+    public Double getR2(){
         return r*r;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[]args){
-        double[] x;
-        double[] y;
-
         Scanner scan = new Scanner(System.in);
 
-        String[] xvalues = {};
-        String[] yvalues = {};
+        System.out.println("Please enter each datapoint as \"x,y\". When you are done, please type \"done\"");
 
-        try {
-            xvalues = scan.nextLine().replace(" ", "").split(",");
-            yvalues = scan.nextLine().replace(" ", "").split(",");
-        } catch(Exception e){
-            System.err.println("Usage: Make sure to only write numbers with commas to separate the datapoints!");
-            System.exit(1);
-        }
-
-        if (xvalues.length!=yvalues.length){
-            System.err.println("Usage: Make sure the x and y datapoints have the same number of elements!");
-            System.exit(1);
-        }
-
-        x = new double[xvalues.length];
-        y = new double[xvalues.length];
-
-        for (int i = 0; i < xvalues.length; i++){
-            x[i] = Double.parseDouble(xvalues[i]);
-            y[i] = Double.parseDouble(yvalues[i]);
+        ArrayList<Double> x = new ArrayList<>();
+        ArrayList<Double> y = new ArrayList<>();
+        String entry = scan.nextLine();
+        while (!entry.equals("done") && !entry.equals("Done")){
+            x.add(Double.parseDouble(entry.split(",")[0]));
+            y.add(Double.parseDouble(entry.split(",")[1]));
+            entry = scan.nextLine();
         }
 
         // x = {};
