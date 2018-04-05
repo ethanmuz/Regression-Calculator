270b6b40c27f4b5d70c43304f7444990fe06f85f
Ethan Uzarowski <ethanmuz@gmail.com>
2018-04-04 22:37:00 -0400

    Now you enter datapoints via command line

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[]args){
-        double[] x = {17,13,12,15,16,14,16,16,18,19};
-        double[] y = {94,73,59,80,93,85,66,79,77,91};
+        double[] x;
+        double[] y;
+
+        Scanner scan = new Scanner(System.in);
+
+        String[] xvalues = {};
+        String[] yvalues = {};
+
+        try {
+            xvalues = scan.nextLine().replace(" ", "").split(",");
+            yvalues = scan.nextLine().replace(" ", "").split(",");
+        } catch(Exception e){
+            System.err.println("Usage: Make sure to only write numbers with commas to separate the datapoints!");
+            System.exit(1);
+        }
+
+        if (xvalues.length!=yvalues.length){
+            System.err.println("Usage: Make sure the x and y datapoints have the same number of elements!");
+            System.exit(1);
+        }
+
+        x = new double[xvalues.length];
+        y = new double[xvalues.length];
+
+        for (int i = 0; i < xvalues.length; i++){
+            x[i] = Double.parseDouble(xvalues[i]);
+            y[i] = Double.parseDouble(yvalues[i]);
+        }
+
+        // x = {};
+        // y = {};
 
         LinearRegression LR = new LinearRegression(x, y);
         System.out.println("y = " + LR.getSlope() + "x + " + LR.getyIntercept());
